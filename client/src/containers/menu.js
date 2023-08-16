9694d605be46edb6bafd34f2d520053dcdca4812
lin.zl <lin.zl@dtwave-inc.com>
2023-08-16 17:32:38 +0800

    feat(init): init the client

---
+++ client/src/containers/menu.js
@@ -0,0 +1,16 @@
+const menu = [
+    {
+        key: '/index',
+        title: '首页',
+        icon: 'home',
+        auth: [1]
+    },
+    {
+        key: '/index',
+        title: '首页',
+        icon: 'home',
+        auth: [1]
+    }
+]
+
+export default menu
