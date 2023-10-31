15abb7aa283f58f1293990ab02f40f25bf6e3857
lin.zl <leo.zl>
2023-10-31 23:23:38 +0800

    feat(code): adjust page code

--- backend/src/main/java/cn/zl/account/book/controller/request/FundsRecordClassifyRequest.java
+++ backend/src/main/java/cn/zl/account/book/controller/request/FundsRecordClassifyRequest.java
@@ -3,6 +3,7 @@ package cn.zl.account.book.controller.request;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author lin.zl
@@ -21,7 +22,7 @@ public class FundsRecordClassifyRequest {
      * 0:支出
      * 1:收入
      */
-    @NotBlank(message = "分类名称不能为空")
+    @NotNull(message = "分类名称不能为空")
     private Integer classifyType;
 
     /**
